423328125ca7650ccb52479151f4ef797197f875
Sergey Malyshev <malyshevs_v@mail.ru>
2021-05-10 20:36:33 +0300

    HttpServletRequest @RequestParam

--- src/main/java/ru/smal/controllers/FirstController.java
+++ src/main/java/ru/smal/controllers/FirstController.java
@@ -3,18 +3,30 @@ package ru.smal.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surename = request.getParameter("surename");
+
+        System.out.println(name + " " + surename + " " + "Hello");
+
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surename", required = false) String surename){
+
+        System.out.println("Hello, " + name + " " + surename);
+
         return "first/goodbye";
     }
 }
